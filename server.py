8338ac4ef036d2d8764e6c6af1efbf4653b7ffb8
Jinsung-L <js99028@gmail.com>
2017-09-29 03:01:33 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, send_from_directory, redirect, Response, stream_with_context
+import requests
+
+app = Flask(__name__)
+MAS_URI = "http://17.253.87.205/"
+
+
+@app.route("/", defaults={'filename': ''})
+@app.route("/<path:filename>")
+def download(filename):
+    try:
+        if len(request.args) > 0:
+            raise Exception("Parameter is given.")
+
+        return send_from_directory("storage", filename)
+
+    except Exception as e:
+        alt_url = MAS_URI + filename
+        req = requests.get(alt_url, stream=True, headers={"Host": "swcdn.apple.com"})
+        return Response(stream_with_context(req.iter_content()), headers=dict(req.headers))
+        # app.logger.info("from MAS: %s" % filename)
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 80, debug=True)
